eb384bf392f4b349e707517d9d3c9847f73aaf22
zrfgame <987907450@qq.com>
2018-11-28 21:26:59 +0800

    首次提交eureka
    Signed-off-by: zrfgame <987907450@qq.com>

---
+++ src/main/java/cn/com/main/eurekaMain.java
@@ -0,0 +1,13 @@
+package cn.com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //打开注册中心
+@SpringBootApplication //全局扫描springboot注解
+public class eurekaMain {
+	public static void main(String[] args) {
+		SpringApplication.run(eurekaMain.class, args);
+	}
+}
